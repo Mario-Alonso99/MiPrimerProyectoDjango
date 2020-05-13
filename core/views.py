a599c6ebab840414237d635e903eec207b9ba627
Mario Alonso <mario.blazter99@gmail.com>
2020-05-13 18:54:48 -0500

    Cuarto Commit: Vid, Extendiendo la App y Usando mi Primer Navegador

--- core/views.py
+++ core/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render, HttpResponse
 
+html_base ="""
+<h1>Mi Web Personal</h1><h2>Portada</h2>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me">Acerca de</a></li>
+</ul>
+"""
+
+
 # Create your views here.
 def home(request):
-    return HttpResponse ("<h1>Mi Web Personal</h1><h2>Portada</h2>")
+    return HttpResponse (html_base + """
+            <h2>Portada</h2>
+            <p>Esto es la Portada</p>
+    """)
+
+def about(request):
+    return HttpResponse (html_base + """
+            <h2>Acerca de</h2>
+            <p>Me Llamo Mario y soy Desarrollador BackEnd.</p>
+    """)
