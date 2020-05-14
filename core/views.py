1083e6dcf1e029d5f1b1002b18893deb25132370
Mario Alonso <mario.blazter99@gmail.com>
2020-05-13 19:27:30 -0500

    Septimo Commit: Vid, Termino de uso de Templates para cada Vista

--- core/views.py
+++ core/views.py
@@ -1,34 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
-html_base ="""
-<h1>Mi Web Personal</h1><h2>Portada</h2>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portfolio/">Portafolio</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-</ul>
-"""
-
-
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse (html_base + """
-            <h2>Acerca de</h2>
-            <p>Me Llamo Mario y soy Desarrollador BackEnd.</p>
-    """)
+    return render(request, "core/about.html")
 
 def portfolio(request):
-    return HttpResponse (html_base + """
-            <h2>Portafolio</h2>
-            <p>Algunos de mis Trabajos</p>
-    """)
+    return render(request, "core/portfolio.html")
 
 def contact(request):
-    return HttpResponse (html_base + """
-            <h2>Contacto</h2>
-            <p>Aquí dejo mi Email para preguntarme sobre cualquier duda: <a href="mario.blazter99@gmail.com">mario.blazter99@gmail.com</a></p>
-    """)
+    return render(request, "core/contact.html")
