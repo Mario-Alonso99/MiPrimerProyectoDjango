ac7019aa05571145b7177892072e25f720b94360
Mario Alonso <mario.blazter99@gmail.com>
2020-05-13 19:07:43 -0500

    Quinto Commit: Vid, Extendiendo la App y Agregando las Vistas de Portafolio y Contacto

--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ html_base ="""
 <ul>
     <li><a href="/">Portada</a></li>
     <li><a href="/about-me">Acerca de</a></li>
+    <li><a href="/portfolio">Portafolio</a></li>
+    <li><a href="/contact">Contacto</a></li>
 </ul>
 """
 
@@ -20,4 +22,16 @@ def about(request):
     return HttpResponse (html_base + """
             <h2>Acerca de</h2>
             <p>Me Llamo Mario y soy Desarrollador BackEnd.</p>
+    """)
+
+def portfolio(request):
+    return HttpResponse (html_base + """
+            <h2>Portafolio</h2>
+            <p>Algunos de mis Trabajos</p>
+    """)
+
+def contact(request):
+    return HttpResponse (html_base + """
+            <h2>Contacto</h2>
+            <p>Aquí dejo mi Email para preguntarme sobre cualquier duda: <a href="mario.blazter99@gmail.com">mario.blazter99@gmail.com</a></p>
     """)
