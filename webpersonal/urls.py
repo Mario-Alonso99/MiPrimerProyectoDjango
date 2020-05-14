744c2f90b629af5ecd0a540b1d9f1499d0f3979d
Mario Alonso <mario.blazter99@gmail.com>
2020-05-14 18:38:24 -0500

    Treceavo commit: Vid, Creando y Administrando el Panel de Administrador de Django

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
 
 urlpatterns = [
@@ -22,5 +22,5 @@ urlpatterns = [
     path('about-me/', views.about, name="about"),
     path('portfolio/', views.portfolio, name="portfolio"),
     path('contact/', views.contact, name="contact"),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin"),
 ]
